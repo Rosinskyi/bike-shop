624d78a39e03a534cd37a67e79ccba7728b82207
Rosinskyi <serhii.rosinskyi@gmail.com>
2022-11-14 13:37:13 +0200

    update dependencies

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,12 @@
 
 export const environment = {
   firebase: {
-    projectId: 'foxminded-9d4fb',
-    appId: '1:413153061288:web:1d0dc38c9b76260928159d',
-    storageBucket: 'foxminded-9d4fb.appspot.com',
-    locationId: 'europe-west',
     apiKey: 'AIzaSyCeG6paWApwXxmnQphe7buBrTQ8ukbh7_0',
     authDomain: 'foxminded-9d4fb.firebaseapp.com',
+    projectId: 'foxminded-9d4fb',
+    storageBucket: 'foxminded-9d4fb.appspot.com',
     messagingSenderId: '413153061288',
+    appId: '1:413153061288:web:1d0dc38c9b76260928159d',
     measurementId: 'G-LWFBCNPG68',
   },
   production: false,
